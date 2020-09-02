aee90a8f66e3643ffad41e775f5637d324d0ad10
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-02 16:27:49 +0300

    acount create form finish

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,39 @@ import './Login.css'
 import {Link, useHistory} from 'react-router-dom'
 import {auth} from '../firebase'
 
+import {makeStyles} from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+
+//Modal Form
+function rand() {
+  return Math.round(Math.random() * 20) - 10
+}
+
+function getModalStyle() {
+  const top = 50 + rand()
+  const left = 50 + rand()
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 350,
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid lightgray',
+    boxShadow: theme.shadows[5],
+  },
+}))
+
 function Login() {
   const history = useHistory()
-
   const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   function login(e) {
@@ -28,10 +57,27 @@ function Login() {
       .createUserWithEmailAndPassword(email, password)
       .then(auth => {
         //acount created , redirect to homepage
+        auth.user.updateProfile({
+          displayName: username,
+        })
         history.push('/')
       })
       .catch(e => alert(e.message))
   }
+
+  //modal requirement
+  const classes = useStyles()
+  const [modalStyle] = React.useState(getModalStyle)
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  //end modal state
 
   return (
     <div className='login'>
@@ -42,7 +88,6 @@ function Login() {
           alt='logo'
         />
       </Link>
-
       <div className='login__container'>
         <h1>Sign in</h1>
         <form action=''>
@@ -56,10 +101,36 @@ function Login() {
           </button>
         </form>
         <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
-        <button className='login__registerButton' onClick={register}>
+        <button className='login__registerButton' onClick={handleOpen}>
           Create Your Amazon Acount
         </button>
       </div>
+
+      {/* Modal Form */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <div className='login__container'>
+            <h1>Sign Up</h1>
+            <form action=''>
+              <h5>Username</h5>
+              <input type='username' value={username} onChange={e => setUsername(e.target.value)} />
+              <h5>Email</h5>
+              <input type='email' value={email} onChange={e => setEmail(e.target.value)} />
+              <h5>Password</h5>
+              <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+              <button type='submit' onClick={register} className='login__signInButton'>
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
